6ca9a8d597886fd4a479426962db6d9e5d4f580d
vinxgradxv <335040@niuitmo.ru>
2022-06-01 10:18:17 +0300

    ыыы

---
+++ client/src/main/java/client/LoginApp.java
@@ -0,0 +1,23 @@
+package client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApp.class.getResource("login1.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ client/src/main/java/client/LoginController.java
@@ -0,0 +1,24 @@
+package client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    public TextField textField;
+    @FXML
+    protected Button log_in_button;
+
+    @FXML
+    protected void onLogInButtonClick(ActionEvent e){
+
+    }
+
+    @FXML
+    protected void onSignUpButtonClick(ActionEvent e){
+
+    }
+
+}
